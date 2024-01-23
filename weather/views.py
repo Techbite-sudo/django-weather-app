5f7e7771ea5f170cc2e06628e05e8d9e07679c18
Techbite <bonfacemwema7@gmail.com>
2024-01-23 07:38:13 +0300

    weather template implementation

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import urllib
+import json
 
 # Create your views here.
+def index(request):
+    if request.method == "POST":
+        if "city" in request.POST:
+            city = request.POST["city"]
+            res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=cb771e45ac79a4e8e2205c0ce66ff633').read()
+        else:
+            city = ""
+            
+    return render(request, "index.html", {})
